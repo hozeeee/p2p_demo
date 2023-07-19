2aff1b35b74710232ba8bb1848f52ee92931beba
hozeeee <15820676504@139.com>
2023-07-19 17:37:32 +0800

    feat: 信息交换自动化

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { Socket } from 'socket.io-client';
-import { ref, onMounted, reactive } from 'vue';
+import { ref, onMounted, reactive, watch } from 'vue';
 import { io } from 'socket.io-client';
 import { ElMessage } from 'element-plus';
 
@@ -10,108 +10,60 @@ const roomId = ref('');
 const inRoom = ref(false);
 const otherInRoom = ref(false);
 const channelData = ref('');
+// 初始化 RTCPeerConnection
+const connectionRef = ref<RTCPeerConnection>();
+const channelRef = ref<RTCDataChannel>();
+const candidateList = reactive<(RTCIceCandidate | null)[]>([]); // TODO:成功建立p2p后清空
 
 
 console.log('location.host: ', location.host)
 
 
 function initSocket() {
-  if (!socket.value) {
-    socket.value = io(`ws://${location.host}/`, { autoConnect: false });
-    socket.value.on('connect', () => {
-      socketConnected.value = true;
-      console.log(`connect: ${socket.value?.connected}`); // true
-    });
-    socket.value.on('disconnect', async () => {
-      socketConnected.value = false;
-      // await new Promise(resolve => setTimeout(resolve, RETRY_INTERVAL));
-      // console.log(`[${dateFormatter()}]尝试重连...`);
-      // socket.value.connect();
-    });
-    socket.value.on('connect_error', async () => {
-      socketConnected.value = false;
-      // await new Promise(resolve => setTimeout(resolve, RETRY_INTERVAL));
-      // socket.value.connect();
-    });
+  // socket.value.connect();
+  if (socket.value) return;
 
-    socket.value.on('other_into_room', () => {
-      otherInRoom.value = true;
-      ElMessage.success('其他人进入房间了');
+  socket.value = io(`ws://${location.host}/`, { autoConnect: true });
+  socket.value.on('connect', () => {
+    socketConnected.value = true;
+    ElMessage.success(`connect: ${socket.value?.connected}`);
+  });
+  socket.value.on('disconnect', async () => {
+    socketConnected.value = false;
+    ElMessage.success('socket disconnect');
+  });
+  socket.value.on('connect_error', async () => {
+    socketConnected.value = false;
+    ElMessage.error('socket connect_error');
+  });
+  // 成功匹配房间号后，分别创建 offer 和 answer
+  socket.value.on('create_offer', createOffer);
+  socket.value.on('create_answer', createAnswer);
+  // 交换 ice_candidate
+  socket.value.on('add_ice_candidate', (candidate: RTCIceCandidate | null) => {
+    console.log('接收到对端的 candidate: ', JSON.stringify(candidate))
+    // if (!candidate) return;
+    if (!connectionRef.value) return ElMessage.error('缺少 connection');
+    connectionRef.value.addIceCandidate(candidate!).then(
+      () => console.log('AddIceCandidate success.'),
+      (err) => console.log('[Failed to add Ice Candidate]: ' + err.toString())
+    );
+  });
+  // 交换 description
+  socket.value.on('receive_desc', (desc: RTCSessionDescriptionInit) => {
+    console.log('接收到对端的 desc: ', desc)
+    if (!desc) return;
+    if (!connectionRef.value) return ElMessage.error('缺少 connection');
+    connectionRef.value.setRemoteDescription(desc).then(
+      () => console.log('setRemoteDescription success.'),
+      (err) => console.log('[Failed to setRemoteDescription]:' + err.toString())
+    );
+  });
 
-      initPeerConnection();
-    });
-
-    socket.value.on('add_ice_candidate', (candidate: RTCIceCandidate | null) => {
-      console.log('接收到对端的 candidate: ', JSON.stringify(candidate))
-      // if (!candidate) return;
-      if (!connectionRef.value) return ElMessage.error('缺少 connection');
-      connectionRef.value.addIceCandidate(candidate!).then(
-        () => console.log('AddIceCandidate success.'),
-        (err) => console.log('[Failed to add Ice Candidate]: ' + err.toString())
-      );
-    });
-
-    socket.value.on('receive_desc', (desc: RTCSessionDescriptionInit) => {
-      console.log('接收到对端的 desc: ', desc)
-      if (!desc) return;
-      if (!connectionRef.value) return ElMessage.error('缺少 connection');
-      connectionRef.value.setRemoteDescription(desc).then(
-        () => console.log('setRemoteDescription success.'),
-        (err) => console.log('[Failed to setRemoteDescription]:' + err.toString())
-      );
-    });
-
-  }
-  socket.value.connect();
+  initPeerConnection();
 }
 onMounted(initSocket);
 
-function getAllUser() {
-  if (!socket.value) return;
-  socket.value.once('all_user_ack', (data) => {
-    console.log('all_user_ack: ', data)
-  })
-  socket.value.emit('all_user')
-}
-
-function getAllRoom() {
-  if (!socket.value) return;
-  socket.value.once('all_room_ack', (data) => {
-    console.log('all_room_ack: ', data)
-  })
-  socket.value.emit('all_room')
-}
-
-
-function createRoom() {
-  if (!socket.value) return;
-  if (!roomId.value) return ElMessage.error('缺少房间号');
-  socket.value.once('create_room_ack', (data) => {
-    console.log('create_room_ack: ', data)
-    const { success, msg } = data;
-    if (!success) return ElMessage.error(msg);
-    inRoom.value = true;
-  });
-  socket.value.emit('create_room', roomId.value);
-}
-
-function intoRoom() {
-  if (!socket.value) return;
-  if (!roomId.value) return ElMessage.error('缺少房间号');
-  socket.value.once('into_room_ack', (data) => {
-    console.log('into_room_ack: ', data)
-    const { success, msg } = data;
-    if (!success) return ElMessage.error(msg);
-    inRoom.value = true;
-  });
-  socket.value.emit('into_room', roomId.value);
-}
-
-
-// 初始化 RTCPeerConnection
-const connectionRef = ref<RTCPeerConnection>();
-const channelRef = ref<RTCDataChannel>();
-const candidateList = reactive<(RTCIceCandidate | null)[]>([]);
 function initPeerConnection() {
   // 创建 [WebRTC] 实例
   const connection = new RTCPeerConnection({
@@ -126,15 +78,10 @@ function initPeerConnection() {
   connectionRef.value = connection
   // 得到 [ICE candidate]
   connectionRef.value.onicecandidate = (evt: RTCPeerConnectionIceEvent) => {
-    console.log('onicecandidate: ', evt)
-    // TODO: print
-    // 发送给对端 connectionRef.value.addIceCandidate
-    if (!socket.value) return ElMessage.error('socket 未初始化');
+    console.log('onicecandidate: ', evt) // TODO:del
     const { candidate } = evt;
-    // 对端监听 add_ice_candidate
-    // socket.value.emit('send_ice_candidate', candidate);
+    // 先存放，因为 addIceCandidate 前必须先执行 setRemoteDescription
     candidateList.push(candidate);
-    // TODO: log   once
   };
   console.log('connection: ', connectionRef.value)
 
@@ -145,78 +92,142 @@ function initPeerConnection() {
    */
 }
 
+function onChannelMessage(evt: MessageEvent) {
+  console.log('Received Message', evt.data);
+  ElMessage.info(evt.data);
+  ElMessage.info(JSON.stringify(evt.data))
+}
+function onChannelStateChange(channel: RTCDataChannel) {
+  const readyState = channel.readyState;
+  console.log(`Channel state is: ${readyState}`);
+}
+function onSocketAck(data: { msg?: string, success: boolean }) {
+  const { success, msg } = data;
+  if (!success) return ElMessage.error(msg);
+  inRoom.value = true;
+}
+function asyncSendSocketData(params: { socket: Socket, event: string, dataList: any[], timeout?: number }) {
+  const { socket, event, dataList, timeout, } = params;
+  const ackEvent = `${event}_ack`;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      socket.off(ackEvent);
+      reject(`socket send timeout (${event})`);
+    }, timeout || 2000);
+    socket.once(ackEvent, (data) => {
+      onSocketAck(data);
+      resolve(data);
+    });
+    socket.emit(event, ...dataList);
+  });
+}
+
+
+// function createRoom() {
+//   if (!socket.value) return;
+//   if (!roomId.value) return ElMessage.error('缺少房间号');
+//   socket.value.once('create_room_ack', onSocketAck);
+//   socket.value.emit('create_room', roomId.value);
+// }
+
+function intoRoom() {
+  if (!socket.value) return;
+  if (!roomId.value) return ElMessage.error('缺少房间号');
+  socket.value.once('into_room_ack', onSocketAck);
+  socket.value.emit('into_room', roomId.value);
+}
+
+
+
 const offerCreated = ref(false);
 const answerCreated = ref(false);
 const localDescription = ref<RTCSessionDescriptionInit>();
-function createOffer() {
-  if (!connectionRef.value) return ElMessage.error('缺少 connection')
 
-  // 创建[发送通道]
-  const sendChannel = connectionRef.value.createDataChannel('stun-channel-2');
-  channelRef.value = sendChannel;
-  console.log('sendChannel: ', sendChannel)
-  // [发送通道]的事件
-  sendChannel.onopen = onSendChannelStateChange;
-  sendChannel.onclose = onSendChannelStateChange;
-  function onSendChannelStateChange() {
-    const readyState = sendChannel.readyState;
-    console.log('Send channel state is: ' + readyState); // TODO: print
-    if (readyState === 'open') {
-      //
-    } else {
-      //
-    }
+
+async function createOffer() {
+  try {
+    if (!connectionRef.value) throw '缺少 connection';
+    // 创建[发送通道]
+    const sendChannel = connectionRef.value.createDataChannel('stun-channel-2');
+    channelRef.value = sendChannel;
+    console.log('channel: ', sendChannel)
+    // [发送通道]的事件
+    sendChannel.onmessage = onChannelMessage;
+    sendChannel.onopen = onChannelStateChange.bind(undefined, sendChannel);
+    sendChannel.onclose = onChannelStateChange.bind(undefined, sendChannel);
+    // 创建[Offer]
+    const desc = await connectionRef.value.createOffer();
+    connectionRef.value!.setLocalDescription(desc);
+    // 通知对端创建[Answer] 发送[Description]
+
+
+    console.log('xxxx: 发送 offer-desc (开始)')
+    await asyncSendSocketData({ socket: socket.value as Socket, event: 'send_desc', dataList: [desc] });
+    console.log('xxxx: 发送 offer-desc (结束)')
+    localDescription.value = desc;
+    offerCreated.value = true;
+
+    // await sendDesc();
+
+    // await new Promise(resolve => setTimeout(resolve, 5 * 1000));
+    // 通知对端，创建[Answer]
+    if (!socket.value) throw '缺少 socket';
+    await asyncSendSocketData({ socket: socket.value as Socket, event: 'inform_create_answer', dataList: [roomId.value] });
+    // socket.value.once('inform_create_answer_ack', onSocketAck);
+    // socket.value.emit('inform_create_answer', roomId.value);
+  } catch (err) {
+    console.error(`[createOffer error]: ${String(err)}`);
+    ElMessage.error(`[createOffer error]: ${String(err)}`);
   }
-
-  connectionRef.value.createOffer().then(
-    (desc) => {
-      console.log('[Offer from localConnection]: ', desc, connectionRef.value);
-      // TODO: 是否需要等 onicecandidate 都结束？
-      connectionRef.value!.setLocalDescription(desc);
-      localDescription.value = desc;
-      offerCreated.value = true;
-    },
-    (error) => { console.log('[Failed to create session description]: ' + error.toString()) }
-  );
 }
-function createAnswer() {
-  if (!connectionRef.value) return ElMessage.error('缺少 connection')
-
-  connectionRef.value.ondatachannel = (event) => {
-    console.log('Receive Channel Callback');
-    const receiveChannel = event.channel;
-    receiveChannel.onmessage = (event) => {
-      console.log('Received Message', event.data);
-      ElMessage.info(event.data);
-      ElMessage.info(JSON.stringify(event.data))
+async function createAnswer() {
+  try {
+    if (!connectionRef.value) throw '缺少 connection';
+    connectionRef.value.ondatachannel = (event) => {
+      // 获取[发送通道]
+      const receiveChannel = event.channel;
+      channelRef.value = receiveChannel;
+      console.log('channel: ', receiveChannel)
+      // [发送通道]的事件
+      receiveChannel.onmessage = onChannelMessage;
+      receiveChannel.onopen = onChannelStateChange.bind(undefined, receiveChannel);
+      receiveChannel.onclose = onChannelStateChange.bind(undefined, receiveChannel);
     };
-    receiveChannel.onopen = onReceiveChannelStateChange;
-    receiveChannel.onclose = onReceiveChannelStateChange;
-    function onReceiveChannelStateChange() {
-      const readyState = receiveChannel.readyState;
-      console.log(`Receive channel state is: ${readyState}`);
-    }
-  };
 
-  connectionRef.value.createAnswer().then(
-    (desc) => {
-      console.log('[Answer from localConnection]: ', desc, connectionRef.value);
-      connectionRef.value!.setLocalDescription(desc);
-      localDescription.value = desc;
-      answerCreated.value = true;
-    },
-    (error) => { console.log('[Failed to create session description]: ' + error.toString()) }
-  );
+    const desc = await connectionRef.value.createAnswer();
+    console.log('[Answer from localConnection]: ', desc, connectionRef.value);
+    connectionRef.value!.setLocalDescription(desc);
+
+    console.log('xxxx: 发送 answer-desc (开始)')
+    await asyncSendSocketData({ socket: socket.value as Socket, event: 'send_desc', dataList: [desc] });
+    console.log('xxxx: 发送 answer-desc (结束)')
+    localDescription.value = desc;
+    answerCreated.value = true;
+
+  } catch (err) {
+    console.error(`[createAnswer error]: ${String(err)}`);
+    ElMessage.error(`[createAnswer error]: ${String(err)}`);
+  }
 }
-function sendDesc() {
+async function sendDesc() {
   if (!socket.value) return ElMessage.error('socket 未初始化');
   if (!localDescription.value) return ElMessage.error('缺少 localDescription');
   // 对端必须先设置了 description 才能正常设置 candidate
-  socket.value.emit('send_desc', localDescription.value);
-  for (const candidate of candidateList) {
-    socket.value.emit('send_ice_candidate', candidate);
-  }
+  // socket.value.emit('send_desc', localDescription.value);
+  await asyncSendSocketData({ socket: socket.value as Socket, event: 'send_desc', dataList: [localDescription.value] });
+  // for (const candidate of candidateList) {
+  //   // socket.value.emit('send_ice_candidate', candidate);
+  //   await asyncSendSocketData({ socket: socket.value as Socket, sendEventName: 'send_ice_candidate', ackEventName: 'send_ice_candidate_ack', data: candidate });
+  // }
+  console.log('candidateList: ', candidateList)
 }
+watch([() => candidateList, localDescription], async () => {
+  if (!localDescription.value || !candidateList.length) return;
+  const candidate = candidateList[0];
+  console.log('xxxx: 发送 candidate', candidateList.length)
+  await asyncSendSocketData({ socket: socket.value as Socket, event: 'send_ice_candidate', dataList: [candidate] });
+  candidateList.shift();
+}, { deep: true });
 
 function sendChannel() {
   if (!channelRef.value) return ElMessage.error('缺少 channelRef');
@@ -224,18 +235,38 @@ function sendChannel() {
   channelRef.value.send(channelData.value);
 }
 
+/**
+ * 网上找的 stun 服务器，google 肯定有效，其他估计不一定有效:
+    stun:stun1.l.google.com:19302
+    stun:stun2.l.google.com:19302
+    stun:stun3.l.google.com:19302
+    stun:stun4.l.google.com:19302
+    stun:23.21.150.121
+    stun:stun01.sipphone.com
+    stun:stun.ekiga.net
+    stun:stun.fwdnet.net
+    stun:stun.ideasip.com
+    stun:stun.iptel.org
+    stun:stun.rixtelecom.se
+    stun:stun.schlund.de
+    stun:stunserver.org
+    stun:stun.softjoys.com
+    stun:stun.voiparound.com
+    stun:stun.voipbuster.com
+    stun:stun.voipstunt.com
+    stun:stun.voxgratia.org
+    stun:stun.xten.com
+ */
+
 </script>
 
 <template>
   <div class="w-full h-full">
 
-    <el-button @click="getAllUser">获取所有用户</el-button>
-    <el-button @click="getAllRoom">获取所有房间</el-button>
-
     <div>
       <span>room: </span>
       <el-input v-model="roomId" :disabled="inRoom" class="!w-200px" />
-      <el-button @click="createRoom" :disabled="inRoom">创建并进入房间</el-button>
+      <!-- <el-button @click="createRoom" :disabled="inRoom">创建并进入房间</el-button> -->
       <el-button @click="intoRoom" :disabled="inRoom">进入房间</el-button>
     </div>
 
--- server/src/socket/web_page.controller.ts
+++ server/src/socket/web_page.controller.ts
@@ -12,9 +12,7 @@ import { Context, Application as SocketApplication } from '@midwayjs/socketio';
 import { spawn } from 'node:child_process';
 
 // TODO: 其实需要删除无用的房间号，但存放的数据并不大，先不管了....
-const ROME_MAP: Record<string, {
-  userIds: string[]
-}> = {};
+const ROOM_MAP: Record<string, { userIds: string[] }> = {};
 
 // 写正则就报错，其他由自制脚本生成 (create_proxy_controller.js)
 @WSController('/')
@@ -31,34 +29,56 @@ export class WebPageSocketController {
   @OnWSDisConnection()
   async onDisConnectionMethod() {
     console.log('onDisConnectionMethod', this.ctx.id);
+    const roomId = this.getRoomIdBySocketId(this.ctx.id);
+    if (roomId && ROOM_MAP[roomId]) delete ROOM_MAP[roomId];
   }
 
-  @OnWSMessage('create_room')
-  @WSEmit('create_room_ack')
-  async createRoom(roomName: string) {
-    if (!roomName)
-      return {
-        success: false,
-        msg: '必须提供房间号',
-      };
-    if (ROME_MAP[roomName])
-      return {
-        success: false,
-        msg: `房间号(${roomName})已存在，不能重复创建`,
-      };
-    ROME_MAP[roomName] = {
-      userIds: [this.ctx.id],
-    };
-    return {
-      success: true,
-    };
-  }
+  // @OnWSMessage('create_room')
+  // @WSEmit('create_room_ack')
+  // async createRoom(roomName: string) {
+  //   if (!roomName)
+  //     return {
+  //       success: false,
+  //       msg: '必须提供房间号',
+  //     };
+  //   if (ROOM_MAP[roomName])
+  //     return {
+  //       success: false,
+  //       msg: `房间号(${roomName})已存在，不能重复创建`,
+  //     };
+  //   ROOM_MAP[roomName] = {
+  //     userIds: [this.ctx.id],
+  //   };
+  //   return {
+  //     success: true,
+  //   };
+  // }
 
   findSocketById(id: string) {
     const allSocket = Object.fromEntries(
       Array.from(this.socketApp.of('/').sockets.entries())
     );
     return allSocket[id];
+  }
+  findRoomOtherSocket(currentUserId: string) {
+    for (const roomId in ROOM_MAP) {
+      const userIds = ROOM_MAP[roomId].userIds || [];
+      if (!userIds.includes(currentUserId) || userIds.length === 1) continue;
+      const targetUserId = userIds.find(id => id !== currentUserId);
+      const tgSocket = this.findSocketById(targetUserId);
+      if (tgSocket) return tgSocket;
+    }
+  }
+  getRoomIdBySocketId(socketId: string) {
+    for (const roomId in ROOM_MAP) {
+      const userIds = ROOM_MAP[roomId]?.userIds;
+      if (ROOM_MAP[roomId] && !Array.isArray(userIds)) {
+        delete ROOM_MAP[roomId];
+        continue;
+      }
+      if (userIds.includes(socketId)) return roomId;
+    }
+    return '';
   }
 
   @OnWSMessage('into_room')
@@ -69,19 +89,22 @@ export class WebPageSocketController {
         success: false,
         msg: '必须提供房间号',
       };
-    if (!ROME_MAP[roomName] || !ROME_MAP[roomName].userIds?.length)
-      return {
-        success: false,
-        msg: `房间号(${roomName})不存在，请先创建`,
+    if (!ROOM_MAP[roomName]) {
+      ROOM_MAP[roomName] = {
+        userIds: [this.ctx.id],
       };
-    if (ROME_MAP[roomName].userIds.length > 1)
+      return {
+        success: true,
+      };
+    }
+    if (ROOM_MAP[roomName].userIds.length > 1)
       return {
         success: false,
         msg: `房间号(${roomName})已满`, // 只会有 2 个人在房间
       };
-    ROME_MAP[roomName].userIds.push(this.ctx.id);
+    ROOM_MAP[roomName].userIds.push(this.ctx.id);
     // 通知双方可以开始对话
-    const userIds = ROME_MAP[roomName].userIds;
+    const userIds = ROOM_MAP[roomName].userIds;
     const allSocket = Object.fromEntries(
       Array.from(this.socketApp.of('/').sockets.entries())
     );
@@ -92,50 +115,63 @@ export class WebPageSocketController {
         success: false,
         msg: '非预期情况，找不到其中一方的 socket',
       };
-    peerOneSocket.emit('other_into_room');
-    peerTwoSocket.emit('other_into_room');
+    peerOneSocket.emit('create_offer');
+    // peerTwoSocket.emit('is_answer');
     return {
       success: true,
     };
   }
-
-  @OnWSMessage('all_user')
-  @WSEmit('all_user_ack')
-  async getAllUser() {
-    const userIds = Array.from(this.socketApp.of('/').sockets.keys());
-    return JSON.stringify(userIds);
+  @OnWSMessage('inform_create_answer')
+  @WSEmit('inform_create_answer_ack')
+  async informCreateAnswer(roomId: string) {
+    if (!roomId || !ROOM_MAP[roomId])
+      return {
+        success: false,
+        msg: '房间号找不到',
+      };
+    if (ROOM_MAP[roomId].userIds.length !== 2)
+      return {
+        success: false,
+        msg: `房间号(${roomId})异常人数`, // 只会有 2 个人在房间
+      };
+    const answerSocketId = ROOM_MAP[roomId].userIds.find(
+      id => id !== this.ctx.id
+    );
+    const tgSocket = this.findSocketById(answerSocketId);
+    if (!answerSocketId || !tgSocket)
+      return { success: false, msg: '非预期错误' };
+    tgSocket.emit('create_answer');
+    return { success: true };
   }
 
-  @OnWSMessage('all_room')
-  @WSEmit('all_room_ack')
-  async getAllRoom() {
-    return Object.keys(ROME_MAP);
-  }
+  // @OnWSMessage('all_user')
+  // @WSEmit('all_user_ack')
+  // async getAllUser() {
+  //   const userIds = Array.from(this.socketApp.of('/').sockets.keys());
+  //   return JSON.stringify(userIds);
+  // }
+
+  // @OnWSMessage('all_room')
+  // @WSEmit('all_room_ack')
+  // async getAllRoom() {
+  //   return Object.keys(ROOM_MAP);
+  // }
 
   @OnWSMessage('send_ice_candidate')
+  @WSEmit('send_ice_candidate_ack')
   async sendIceCandidate(...args) {
-    console.log('args: ', args)
-    const currentUserId = this.ctx.id;
-    for (const roomId in ROME_MAP) {
-      const userIds = ROME_MAP[roomId].userIds || [];
-      if (!userIds.includes(currentUserId) || userIds.length === 1) continue;
-      const targetUserId = userIds.find(id => id !== currentUserId);
-      const tgSocket = this.findSocketById(targetUserId);
-      tgSocket.emit('add_ice_candidate', ...args);
-    }
-    // TODO: 提示发送成功
+    const tgSocket = this.findRoomOtherSocket(this.ctx.id);
+    if (!tgSocket) return { success: false, msg: '非预期错误' };
+    tgSocket.emit('receive_desc', ...args);
+    return { success: true };
   }
 
   @OnWSMessage('send_desc')
+  @WSEmit('send_desc_ack')
   async sendDesc(...args) {
-    const currentUserId = this.ctx.id;
-    for (const roomId in ROME_MAP) {
-      const userIds = ROME_MAP[roomId].userIds || [];
-      if (!userIds.includes(currentUserId) || userIds.length === 1) continue;
-      const targetUserId = userIds.find(id => id !== currentUserId);
-      const tgSocket = this.findSocketById(targetUserId);
-      tgSocket.emit('receive_desc', ...args);
-    }
-    // TODO: 提示发送成功
+    const tgSocket = this.findRoomOtherSocket(this.ctx.id);
+    if (!tgSocket) return { success: false, msg: '非预期错误' };
+    tgSocket.emit('receive_desc', ...args);
+    return { success: true };
   }
 }
